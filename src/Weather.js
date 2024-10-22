460b6307c8574a5618e8522d03be148f611cc03c
WORKBLYS <margaret@workblis.com>
2024-10-22 13:53:29 +0100

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -10,10 +10,16 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
+
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -22,13 +28,18 @@ export default function Weather() {
         <li>Wednesday 07:00</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy"
-          />
-          6°C
+          <div className="d-flex align-items-center">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Cloudy"
+              className="weather-icon"
+            />
+            <span className="temperature">
+              6<sup className="unit">°C</sup>
+            </span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
