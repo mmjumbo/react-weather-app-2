6ad0424e9614cf589834e7d3482f7e5469fa30a1
WORKBLYS <dev@workblys.com>
2025-02-08 04:04:43 -0500

    created new components to track relevant temperatures in different units.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 
 export default function WeatherInfo(props) {
@@ -20,10 +21,9 @@ export default function WeatherInfo(props) {
               alt={props.data.description}
               className="weather-icon"
             />
-            <span className="temperature">
-              {Math.round(props.data.temperature)}
-            </span>{" "}
-            <span className="unit">°C</span>
+
+            <WeatherTemperature celsius={props.data.temperature} />
+            
           </div>
         </div>
         <div className="col-6">
